bf6f6191457ac9646c9db773baaf325185dd3779
ghadi0alzhrani0 <ghadi0alzhrani@gmail.com>
2026-02-26 10:25:07 +0000

    3-print_alphabets.c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints alphaBETS
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lowerbet = 'a';
+	char alphabet = 'A';
+
+	while (lowerbet <= 'z')
+	{
+		putchar(lowerbet);
+		lowerbet++;
+	}
+	while (alphabet <= 'Z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('\n');
+	return (0);
+}
