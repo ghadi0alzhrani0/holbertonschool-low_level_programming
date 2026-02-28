1408b64151131bfc72a14f53301dd63c339808ec
ghadi0alzhrani0 <ghadi0alzhrani@gmail.com>
2026-02-28 01:44:59 +0000

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: sum of a and b
+ */
+int add(int, int);
+{
+	int result;
+
+	result = a + b;
+
+	return (result);
+}
