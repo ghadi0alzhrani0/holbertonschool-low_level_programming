871e169cf6b333de3617354db32542fdcf793460
ghadi0alzhrani0 <ghadi0alzhrani@gmail.com>
2026-03-06 12:36:03 +0000

    6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -7,11 +7,17 @@
 void puts2(char *str)
 {
 	int i = 0;
+	int len = 0;
 
-	while (str[i] != '\0')
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	while (i < len)
 	{
 		_putchar(str[i]);
-		i = i + 2;
+		i += 2;
 	}
 
 	_putchar('\n');
