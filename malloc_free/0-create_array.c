b9f2999f5c9327d961d1e4571aca154276f2fdc9
ghadi0alzhrani0 <ghadi0alzhrani0@gmail.com>
2026-03-30 08:13:54 +0000

    0-create_array.c

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -10,22 +10,22 @@
  */
 char *create_array(unsigned int size, char c)
 {
-    char *arr;
-    unsigned int i;
+	char *arr;
+	unsigned int i;
 
-    if (size == 0)
-        return (NULL);
+	if (size == 0)
+		return (NULL);
 
-    arr = malloc(sizeof(char) * size);
-    if (arr == NULL)
-        return (NULL);
+	arr = malloc(sizeof(char) * size);
+	if (arr == NULL)
+		return (NULL);
 
-    i = 0;
-    while (i < size)
-    {
-        arr[i] = c;
-        i++;
-    }
+	i = 0;
+	while (i < size)
+	{
+		arr[i] = c;
+		i++;
+	}
 
-    return (arr);
+	return (arr);
 }
