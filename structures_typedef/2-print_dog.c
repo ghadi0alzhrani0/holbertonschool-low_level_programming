0ef7428995a96bab8404775670ab1a70b06547d2
ghadi0alzhrani0 <ghadi0alzhrani@gmail.com>
2026-04-01 09:09:09 +0000

    2-print_dog.c

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints a dog 
+ * @d: the dog to print
+ *
+ * Return:void.
+ */
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		pintf("Name: %\n", d->name ? d->name : "(nil)");
+		printf("Age: %f\n" , d->age);
+		printf("Owner: %s\n" , d->owner ? d->owner : "(nil)");
+	}
+}
