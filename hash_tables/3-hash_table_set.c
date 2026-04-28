5f735092defd571765ed3a15d2046580ec7e4955
ghadi0alzhrani0 <ghadi0alzhrani@gmail.com>
2026-04-28 12:09:52 +0000

    3-hash_table_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -2,56 +2,67 @@
 #include <string.h>
 
 /**
- * hash_table_set - adds a key and value to the hash table
- * @ht: the hash table
- * @key: the key
- * @value: the value
+ * update_node - updates a node value
+ * @node: node
+ * @key: key
+ * @value: value
  *
- * Return: 1 if it works, 0 if it fails
+ * Return: 1 if updated, 0 if not
+ */
+int update_node(hash_node_t *node, const char *key, const char *value)
+{
+	char *copy;
+
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			copy = strdup(value);
+			if (copy == NULL)
+				return (0);
+
+			free(node->value);
+			node->value = copy;
+			return (1);
+		}
+		node = node->next;
+	}
+
+	return (0);
+}
+
+/**
+ * hash_table_set - adds or updates a node
+ * @ht: hash table
+ * @key: key
+ * @value: value
+ *
+ * Return: 1 on success, 0 on failure
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_node;
-	hash_node_t *temp;
 	unsigned long int index;
-	char *new_value;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
 
-	temp = ht->array[index];
-	while (temp != NULL)
-	{
-		if (strcmp(temp->key, key) == 0)
-		{
-			new_value = strdup(value);
-			if (new_value == NULL)
-				return (0);
-
-			free(temp->value);
-			temp->value = new_value;
-			return (1);
-		}
-		temp = temp->next;
-	}
+	if (update_node(ht->array[index], key, value) == 1)
+		return (1);
 
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
 
 	new_node->key = strdup(key);
-	if (new_node->key == NULL)
-	{
-		free(new_node);
-		return (0);
-	}
-
 	new_node->value = strdup(value);
-	if (new_node->value == NULL)
+
+	if (new_node->key == NULL || new_node->value == NULL)
 	{
 		free(new_node->key);
+		free(new_node->value);
 		free(new_node);
 		return (0);
 	}
